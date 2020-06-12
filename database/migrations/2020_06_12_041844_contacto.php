41f7cf911892a593d770cc1ddf321ad778088b4b
hugo <hugo.delacruz@tecsup.edu.pe>
2020-06-11 23:32:25 -0500

    Final and Complit

---
+++ database/migrations/2020_06_12_041844_contacto.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Contacto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacto', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('correo');
+            $table->string('celular');
+            $table->string('direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacto');
+    }
+}

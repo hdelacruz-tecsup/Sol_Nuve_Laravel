01b779baa8801591d816e150c9a84288a4de3df3
hugo <hugo.delacruz@tecsup.edu.pe>
2020-06-11 20:44:33 -0500

    Local Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hugo De La Cruz
+                    Modiciacion Local
                 </div>
 
                 <div class="links">
